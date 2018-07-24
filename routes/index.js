081f99f22e5ecd15d2b0f850ffaf263fe660a3c7
Nick Foden <nickfoden@gmail.com>
2018-07-23 22:35:48 -0400

    Added jsx view engine and started jsx react files in views folder

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "BEYONCE API" });
+  res.render("index", {});
 });
 
 router.get("/vegan_food", function(req, res, next) {
---
+++ views/index.jsx
@@ -0,0 +1,13 @@
+var React = require("react");
+
+class IndexComponent extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> MORE YONCE </h1>
+      </div>
+    );
+  }
+}
+
+module.exports = IndexComponent;
