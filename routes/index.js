35725b8871249998d2ead8a6fb33fb588c0e862d
nickfoden <nickfoden@gmail.com>
2018-07-24 17:55:23 -0400

    Added albums route with some basic information

--- routes/index.js
+++ routes/index.js
@@ -75,6 +75,95 @@ router.get("/facts", function(req, res, next) {
   ]);
 });
 
+router.get("/albums", function(req, res, next) {
+  res.send([
+    {
+      title: "Dangerously In Love",
+      type: "Studio",
+      year: 2003,
+      cover:
+        "https://upload.wikimedia.org/wikipedia/en/8/84/Dangerously_In_Love_Album%282003%29.png"
+    },
+    {
+      title: "B'Day",
+      type: "Studio",
+      year: 2006,
+      cover:
+        "https://upload.wikimedia.org/wikipedia/en/c/c4/Beyonc%C3%A9_-_B%27Day.png"
+    },
+    {
+      title: "I Am... Sasha Fierce",
+      type: "Studio",
+      year: 2008,
+      cover:
+        "https://upload.wikimedia.org/wikipedia/en/9/96/I_Am..._Sasha_Fierce.png"
+    },
+    {
+      title: 4,
+      type: "Studio",
+      year: 2011,
+      cover:
+        "https://upload.wikimedia.org/wikipedia/en/c/ca/Beyonc%C3%A9_-_4.png"
+    },
+    {
+      title: "Beyoncé",
+      type: "Studio",
+      year: 2013,
+      cover:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Beyonc%C3%A9_-_Beyonc%C3%A9.svg/750px-Beyonc%C3%A9_-_Beyonc%C3%A9.svg.png"
+    },
+    {
+      title: "Lemonade",
+      type: "Studio",
+      year: 2016,
+      cover:
+        "https://upload.wikimedia.org/wikipedia/en/5/53/Beyonce_-_Lemonade_%28Official_Album_Cover%29.png"
+    },
+    {
+      title: "Everything Is Love",
+      type: "Collaborative",
+      year: 2018,
+      cover:
+        "https://upload.wikimedia.org/wikipedia/en/3/3d/The_Carters_-_Everything_Is_Love_%28Official_Album_Cover%29.png"
+    },
+    {
+      title: "Live at Wembley",
+      type: "Live",
+      year: 2004,
+      cover:
+        "https://upload.wikimedia.org/wikipedia/en/1/11/Beyonce_-_Live_at_Wembley.png"
+    },
+    {
+      title: "The Beyoncé Experience Live",
+      type: "Live",
+      year: 2007,
+      cover:
+        "https://upload.wikimedia.org/wikipedia/en/c/c9/The_Beyonc%C3%A9_Experience_Live.png"
+    },
+    {
+      title: "I Am... Yours: An Intimate Performance at Wynn Las Vega",
+      type: "Live",
+      year: 2009,
+      cover:
+        "https://upload.wikimedia.org/wikipedia/en/e/e3/I_Am..._Yours_%28CD_Cover_Art%29.png"
+    },
+    {
+      title: "I Am... World Tour",
+      type: "Live",
+      year: 2010,
+      cover:
+        "https://upload.wikimedia.org/wikipedia/en/a/a0/I_am_tour_standard.jpg"
+    },
+    {
+      title: "Dreamgirls",
+      type: "Sountrack",
+      year: 2006,
+      cover:
+        "https://upload.wikimedia.org/wikipedia/en/9/96/DreamgirlsCover.jpg"
+    }
+  ]);
+});
+
 module.exports = router;
 
 // https://stark-badlands-52044.herokuapp.com/fact
